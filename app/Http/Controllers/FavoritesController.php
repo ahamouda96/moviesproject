989b504c9f452770d45a0c5c09a0a9b9436b45ec
Ahmed Hamouda <42685348+ahamouda96@users.noreply.github.com>
2019-05-08 01:35:51 +0200

    Update FavoritesController.php

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -21,26 +21,49 @@ class FavoritesController extends Controller
         return view('favorites',compact('types','mainactor','director','year'));
     }
 
-     public function show()
-    {
+//      public function show()
+//     {
 
-      $array = array();
-      $r=Movie::all();
-      foreach($r as $key => $value) {
+//       $array = array();
+//       $r=Movie::all();
+//       foreach($r as $key => $value) {
         
-         array_push($array, array("score", $value->match())); 
-         array_push($array, array("type", $value->type));
-         array_push($array, array("mainactor", $value->mainactor)); 
-         array_push($array, array("director", $value->director)); 
-         array_push($array, array("year", $value->year));
+//          array_push($array, array("score", $value->match())); 
+//          array_push($array, array("type", $value->type));
+//          array_push($array, array("mainactor", $value->mainactor)); 
+//          array_push($array, array("director", $value->director)); 
+//          array_push($array, array("year", $value->year));
 
            
         
         
-      }
-      return redirect()->back()->with(compact('array'));
+//       }
+//       return redirect()->back()->with(compact('array'));
       
-    }
+//     }
+    
+    
+    $movies = Movie::all();
+
+        foreach ($movies as $movie){
+            
+             $score=($movie->type==($favorite->type)?1:0)+ ($movie->mainactor==($favorite->mainactor)?1:0)+ ($movie->director==($favorite->director)?1:0)+ ($movie->year==($favorite->year)?1:0);
+
+             $score = ($score/4)*100;
+
+             if($score == 100){
+                return view('favoritemovie')->with(compact('score', 'movies'));
+             }
+             if($score == 75){
+                return view('favoritemovie')->with(compact('score', 'movies'));
+             }
+             if($score == 50){
+                return view('favoritemovie')->with(compact('score', 'movies'));
+             }
+             if($score == 25){
+                return view('favoritemovie')->with(compact('score', 'movies'));
+             }
+        }   
 
     
 }
